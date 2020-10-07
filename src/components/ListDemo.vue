b0edd0acce0c0be80b4d8d33c29083d424bb7a9a
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2020-10-07 16:54:54 +0300

    listdemo fixed

--- src/components/ListDemo.vue
+++ src/components/ListDemo.vue
@@ -23,16 +23,8 @@
                                 v-model="sortKey"
                                 placeholder="Sort By Price"
                                 @change="onSortChange($event)"
-                                styleClass="p-mb-2 p-mb-md-0"
+                                class="p-mb-2 p-mb-md-0"
                             ></Dropdown>
-                            <span class="p-input-icon-left p-mb-2 p-mb-md-0">
-                                <i class="pi pi-search"></i>
-                                <InputText
-                                    type="search"
-                                    placeholder="Search by Name"
-                                    @input="dv.filter($event.target.value)"
-                                />
-                            </span>
                             <DataViewLayoutOptions
                                 v-model="layout"
                             ></DataViewLayoutOptions>
@@ -234,7 +226,7 @@ export default {
     },
     methods: {
         onSortChange($event) {
-            const value = $event.value;
+            const value = $event.value.value;
 
             if (value.indexOf("!") === 0) {
                 this.sortOrder = -1;
@@ -249,95 +241,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-/deep/ .p-dropdown {
-    width: 14rem;
-    font-weight: normal;
-}
-
 .product-name {
     font-size: 1.5rem;
     font-weight: 700;
 }
-
 .product-description {
     margin: 0 0 1rem 0;
 }
-
 .product-category-icon {
     vertical-align: middle;
     margin-right: 0.5rem;
 }
-
 .product-category {
     font-weight: 600;
     vertical-align: middle;
 }
-
-.product-list-item {
+/deep/ .product-list-item {
+    display: -ms-flexbox;
     display: flex;
+    -ms-flex-align: center;
     align-items: center;
     padding: 1rem;
     width: 100%;
-
     img {
         width: 150px;
         box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
         margin-right: 2rem;
     }
-
     .product-list-detail {
         flex: 1 1 0;
+        -ms-flex: 1 1 0px;
     }
-
     .p-rating {
         margin: 0 0 0.5rem 0;
     }
-
     .product-price {
         font-size: 1.5rem;
         font-weight: 600;
         margin-bottom: 0.5rem;
         align-self: flex-end;
     }
-
     .product-list-action {
+        display: -ms-flexbox;
         display: flex;
+        -ms-flex-direction: column;
         flex-direction: column;
     }
-
     .p-button {
         margin-bottom: 0.5rem;
     }
-
-    .product-grid-item {
-        margin: 0.5em;
-        border: 1px solid #dee2e6;
-
-        .product-grid-item-top,
-        .product-grid-item-bottom {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-        }
-
-        img {
-            width: 75%;
-            box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16),
-                0 3px 6px rgba(0, 0, 0, 0.23);
-            margin: 2rem 0;
-        }
-
-        .product-grid-item-content {
-            text-align: center;
-        }
-
-        .product-price {
-            font-size: 1.5rem;
-            font-weight: 600;
-        }
-    }
 }
-
 .product-badge {
     border-radius: 2px;
     padding: 0.25em 0.5rem;
@@ -345,50 +298,73 @@ export default {
     font-weight: 700;
     font-size: 12px;
     letter-spacing: 0.3px;
-
     &.status-instock {
         background: #c8e6c9;
         color: #256029;
     }
-
     &.status-outofstock {
         background: #ffcdd2;
         color: #c63737;
     }
-
     &.status-lowstock {
         background: #feedaf;
         color: #8a5340;
     }
 }
-
+/deep/ .product-grid-item {
+    margin: 0.5em;
+    border: 1px solid var(--surface-d);
+    .product-grid-item-top,
+    .product-grid-item-bottom {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        -ms-flex-pack: justify;
+        justify-content: space-between;
+    }
+    img {
+        width: 75%;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+        margin: 2rem 0;
+    }
+    .product-grid-item-content {
+        text-align: center;
+    }
+    .product-price {
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+}
 @media screen and (max-width: 576px) {
     /deep/ .product-list-item {
+        -ms-flex-direction: column;
         flex-direction: column;
+        -ms-flex-align: center;
         align-items: center;
-
         img {
             width: 75%;
             margin: 2rem 0;
         }
-
         .product-list-detail {
             text-align: center;
         }
-
         .product-price {
             align-self: center;
         }
-
         .product-list-action {
+            display: -ms-flexbox;
             display: flex;
+            -ms-flex-direction: column;
             flex-direction: column;
         }
-
         .product-list-action {
             margin-top: 2rem;
+            -ms-flex-direction: row;
             flex-direction: row;
+            -ms-flex-pack: justify;
             justify-content: space-between;
+            -ms-flex-align: center;
             align-items: center;
             width: 100%;
         }
