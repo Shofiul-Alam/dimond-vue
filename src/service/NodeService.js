12d52532fb19fe20a750e3a3c95e075a823bd9d4
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2020-10-20 13:24:30 +0300

    removed unused data and service

--- src/service/NodeService.js
+++ src/service/NodeService.js
@@ -1,35 +1,11 @@
 import axios from 'axios';
 
 export default class NodeService {
-    // getFiles() {
-    //     return axios
-    //         .get("assets/demo/data/files.json")
-    //         .then(res => res.data.data);
-    // }
-
-    // getLazyFiles() {
-    //     return axios
-    //         .get("assets/demo/data/files-lazy.json")
-    //         .then(res => res.data.data);
-    // }
-
-    // getFilesystem() {
-    //     return axios
-    //         .get("assets/demo/data/filesystem.json")
-    //         .then(res => res.data.data);
-    // }
-
-    // getLazyFilesysytem() {
-    //     return axios
-    //         .get("assets/demo/data/filesystem-lazy.json")
-    //         .then(res => res.data.data);
-    // }
-
     getTreeTableNodes() {
-        return axios.get('assets/demo/data/treetablenodes.json').then((res) => res.data.root);
+        return axios.get('assets/demo/data/treetablenodes.json').then(res => res.data.root);
     }
 
     getTreeNodes() {
-        return axios.get('assets/demo/data/treenodes.json').then((res) => res.data.root);
+        return axios.get('assets/demo/data/treenodes.json').then(res => res.data.root);
     }
 }
