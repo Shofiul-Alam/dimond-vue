7c000e2485e40e517534120fce2c8427c4096b66
mertsincan <sincan.mert@gmail.com>
2020-10-21 13:40:35 +0300

    Ripple fix

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ Vue.directive('Tooltip', Tooltip);
 Vue.directive('ripple', Ripple);
 
 Vue.prototype.$appState = Vue.observable({ inputStyle: 'outlined' });
-Vue.prototype.$primevue = Vue.observable({ ripple: false });
+Vue.prototype.$primevue = Vue.observable({ ripple: true });
 
 Vue.config.productionTip = false;
 
