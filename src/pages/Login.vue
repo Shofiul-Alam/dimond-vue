e557a9ce850a4d786e3c23dafe6d52e42d00892b
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-02-22 13:17:10 +0300

    update login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,7 +9,7 @@
 					<p>Already have an account? <a href="/">Login</a></p>
 					<InputText id="name" placeholder="Full Name" />
 					<InputText id="email" placeholder="Email" />
-					<Password placeholder="Password" />
+					<Password placeholder="Password"/>
 					<Button label="CONTINUE" type="button"></Button>
 				</div>
 
