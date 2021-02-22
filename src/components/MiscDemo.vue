8b5330f4a4fc13a7db4dccd1c636cad626d2a5e1
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-02-22 14:36:25 +0300

    scrolltop style added

--- src/components/MiscDemo.vue
+++ src/components/MiscDemo.vue
@@ -213,4 +213,25 @@
 	.p-overlay-badge {
 		margin-right: 2rem;
 	}
+
+    ::v-deep(.custom-scrolltop){
+        width: 2rem;
+        height: 2rem;
+        border-radius: 4px;
+        background-color: var(--primary-color);
+    }
+    ::v-deep(.custom-scrolltop .p-scrolltop-icon) {
+        font-size: 1rem;
+        color: var(--primary-color-text);
+    }
+    ::v-deep(.custom-scrolltop:hover) {
+         background-color: var(--primary-color);
+    }
+    ::v-deep(.custom-skeleton) {
+        border: 1px solid var(--surface-d);
+        border-radius: 4px;
+    }
+    ::v-deep(.custom-skeleton ul) {
+        list-style: none;
+    }
 </style>
