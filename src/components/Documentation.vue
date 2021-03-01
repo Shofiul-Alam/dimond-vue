f39fee1754b455e8a1c7bc4d4ae1f4ab19b361f2
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-03-01 11:05:59 +0300

    update variables

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -1194,6 +1194,17 @@ $splitterGutterHandleBg:$shade300;
     --primary-color:#{$primaryColor};
     --primary-color-text:#{$primaryTextColor};
     --font-family:#{$fontFamily};
+    --surface-0: #ffffff;
+    --surface-50: #FAFAFA;
+    --surface-100: #F5F5F5;
+    --surface-200: #EEEEEE;
+    --surface-300: #E0E0E0;
+    --surface-400: #BDBDBD;
+    --surface-500: #9E9E9E;
+    --surface-600: #757575;
+    --surface-700: #616161;
+    --surface-800: #424242;
+    --surface-900: #212121;
 }
 
 </code></pre>
