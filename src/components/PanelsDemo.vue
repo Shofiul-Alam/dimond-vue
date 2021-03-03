b81aadf79b9ab3920592aa32283ba6f9a678180c
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-03-03 10:00:10 +0300

    fix accordion

--- src/components/PanelsDemo.vue
+++ src/components/PanelsDemo.vue
@@ -24,7 +24,7 @@
 			<div class="card">
 				<h5>AccordionPanel</h5>
 				<Accordion  :activeIndex="0">
-					<AccordionTab header="Header I" :active="true">
+					<AccordionTab header="Header I">
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
